ddc6642762df0b2925d959b282197aa17f4e7012
ErickCarrillo98 <82337484+ErickCarrillo98@users.noreply.github.com>
2021-08-02 11:28:46 -0700

    Create MyInfo.java

---
+++ practice/MyInfo.java
@@ -0,0 +1,7 @@
+public class MyInfo {
+    public static void main(String[] args) {
+        System.out.println("My name is Erick Carrillo");
+        System.out.println("I am 22 years old");
+        System.out.println("I live in wa!!");
+    }
+}
